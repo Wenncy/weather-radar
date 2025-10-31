71c411875f1c073eebf34c8cb7ffbb0123e585f1
Wency Bay <baywencyvicegardose@gmail.com>
2025-10-31 12:53:15 +0800

    Update fetch to use REACT_APP_API_BASE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,16 @@ import "leaflet/dist/leaflet.css";
 import "./App.css";
 
 function App() {
-    useEffect(() => {
-    fetch("http://localhost:3001/api/test")
+  // Use environment variable or fallback to localhost
+  const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:3001";
+
+  useEffect(() => {
+    fetch(`${API_BASE}/api/test`)
       .then(res => res.json())
       .then(data => console.log(data))
       .catch(err => console.error(err));
   }, []);
-  
+
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <MapContainer center={[39, -98]} zoom={4} style={{ height: "100%", width: "100%" }}>
